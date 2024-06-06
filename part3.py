f39762388d6d459356f512dba8e6a3704bbad3c3
Jongyoon-Choi <chg9535@gmail.com>
2024-06-06 21:21:03 +0900

    3번 문제 ver 1.2

--- part3.py
+++ part3.py
@@ -1,121 +1,275 @@
+import random
+import numpy as np
+import pandas as pd
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
+import torch.optim as optim
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+from collections import namedtuple
 
-class QNet(nn.Module):
+from network import QTrainer, QNet
 
-    def __init__(self, emb_dim, T=4, node_dim = 5):
-        super(QNet, self).__init__()
-        self.emb_dim = emb_dim
-        self.T = T
-        self.node_dim = node_dim      
-        
-        self.theta1 = nn.Linear(self.node_dim, self.emb_dim, True)
-        self.theta2 = nn.Linear(self.emb_dim, self.emb_dim, True)
-        self.theta3 = nn.Linear(self.emb_dim, self.emb_dim, True)
-        self.theta4 = nn.Linear(1, self.emb_dim, True)
-        self.theta5 = nn.Linear(2*self.emb_dim, 1, True)
-        self.theta6 = nn.Linear(self.emb_dim, self.emb_dim, True)
-        self.theta7 = nn.Linear(self.emb_dim, self.emb_dim, True)
-        
-        self.layer = nn.Linear(self.emb_dim, self.emb_dim, True) 
-        
-    def forward(self, xv, Ws):
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-        num_nodes = xv.shape[1]   # 전체 도시의 수
-        batch_size = xv.shape[0]  # batch size
-        
-        
-        # distance matrix의 값이 0인 곳은 0으로, 0 이상인 곳은 1로 채운 conn_matries
-        # --> 대각 원소 = 0, 그 외의 원소 = 1
-        conn_matrices = torch.where(Ws > 0, torch.ones_like(Ws), torch.zeros_like(Ws)).to(device)
-        
-        mu = torch.zeros(batch_size, num_nodes, self.emb_dim, device=device)
+# 파라미터 정의
+SEED = 1                     # A seed for the random number generator
+torch.manual_seed(SEED)
+np.random.seed(SEED)
+random.seed(SEED)
 
-        # state 정보를 embedding 
-        s1 = self.theta1(xv)                     # (batch_size, num_nodes, 5) --> (batch_size, num_nodes, emb_dim)
-        s1 = self.layer(F.relu(s1))              # (batch_size, num_nodes, emb_dim) --> (batch_size, num_nodes, emb_dim)
-        
-        # distance matrix 정보를 embedding
-        s3_0 = Ws.unsqueeze(3)                   # (batch_size, num_nodes, num_nodes) --> (batch_size, num_nodes, num_nodes, 1)
-        s3_1 = F.relu(self.theta4(s3_0))         # (batch_size, num_nodes, num_nodes, 1) --> (batch_size, num_nodes, num_nodes, emb_dim)
-        s3_2 = torch.sum(s3_1, dim=1)            # (batch_size, num_nodes, num_nodes, emb_dim) --> (batch_size, num_nodes, emb_dim)
-        s3 = self.theta3(s3_2)                   # (batch_size, num_nodes, emb_dim) --> (batch_size, num_nodes, emb_dim)
-        
+# Graph
+NR_NODES = 998               # Number of nodes N
+EMBEDDING_DIMENSIONS = 5     # Embedding dimension D
+EMBEDDING_ITERATIONS_T = 1   # Number of embedding iterations T
 
-        # state 정보(s1)와 각 state에 대한 나머지 node들의 distance 정보(s3)를 함께 embedding
-        for _ in range(self.T):
-            s2 = self.theta2(conn_matrices.matmul(mu))    # state와 action이 동일한 경우 (대각 원소)를 제외하고 정보 융합
-            mu = F.relu(s1 + s2 + s3)
-            
-        # 전체적인 state와 distance에 대한 정보를 모든 노드에 동일하게 제공
-        global_state = self.theta6(torch.sum(mu, dim=1, keepdim=True).repeat(1, num_nodes, 1))
+# Learning
+NR_EPISODES = 100
+MEMORY_CAPACITY = 10000
+N_STEP_QL = 2                # Number of steps (n) in n-step Q-learning to wait before computing target reward estimate
+BATCH_SIZE = 16
+
+GAMMA = 0.9
+INIT_LR = 5e-3
+LR_DECAY_RATE = 1. - 2e-5    # learning rate decay
+
+MIN_EPSILON = 0.1
+EPSILON_DECAY_RATE = 6e-4    # epsilon decay
+
+"""
+State, action 관련 자료형, 함수, 클래스 정의
+- State : 현재 state에 대한 정보를 저장하기 위한 자료형 
+- Experience : state tensor들을 한번만 계산하기 위해 experience 인스턴스에 저장합니다.  
+- state2tens : state를 5개의 차원으로 embedding하는 함수  
+- 여러 experience를 저장해두기 위한 메모리 클래스  
+"""
+State = namedtuple('State', ('W', 'coords', 'partial_solution'))
+Experience = namedtuple('Experience', ('state', 'state_tsr', 'action', 'reward', 'next_state', 'next_state_tsr'))
+
+
+def state2tens(state):
+    solution = set(state.partial_solution)
+    sol_last_node = state.partial_solution[-1] if len(state.partial_solution) > 0 else -1
+    sol_first_node = state.partial_solution[0] if len(state.partial_solution) > 0 else -1
+    coords = state.coords
+    nr_nodes = coords.shape[0]
+
+    xv = [[(1 if i in solution else 0),           # 해당 노드를 방문 했는지 여부
+           (1 if i == sol_first_node else 0),     # 해당 노드가 시작 노드인지 여부 
+           (1 if i == sol_last_node else 0),      # 해당 노드가 마지막 노드인지 여부
+           coords[i,0],                           # 해당 노드의 x좌표
+           coords[i,1]                            # 해당 노드의 y좌표
+          ] for i in range(nr_nodes)]
+    
+    return torch.tensor(xv, dtype=torch.float32, requires_grad=False, device=device)
+
+
+class Memory(object):
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
+        self.nr_inserts = 0
         
-        # 각각의 state에 대한 정보
-        local_action = self.theta7(mu)  
-            
-        # 전체적인 정보와, 각각의 state에서의 정보를 함께 융합하여 Q-value 예측
-        out = F.relu(torch.cat([global_state, local_action], dim=2))
-        return self.theta5(out).squeeze(dim=2)
+    def remember(self, experience):
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = experience
+        self.position = (self.position + 1) % self.capacity
+        self.nr_inserts += 1
+        
+    def sample_batch(self, batch_size):
+        return random.sample(self.memory, batch_size)
+    
+    def __len__(self):
+        return min(self.nr_inserts, self.capacity)
+
+# 그 외의 함수 정의
+def total_distance(solution, W):
+    if len(solution) < 2:
+        return 0 
+    
+    total_dist = 0
+    for i in range(len(solution) - 1):
+        total_dist += W[solution[i], solution[i+1]].item()
+        
+    if len(solution) == W.shape[0]:
+        total_dist += W[solution[-1], solution[0]].item()
+
+    return total_dist
+
+        
+def is_state_final(state):
+    return len(set(state.partial_solution)) == state.W.shape[0]
+
+
+def get_next_neighbor_random(state):
+    solution, W = state.partial_solution, state.W
+    
+    if len(solution) == 0:
+        return random.choice(range(W.shape[0]))
+    already_in = set(solution)
+    candidates = list(filter(lambda n: n.item() not in already_in, W[solution[-1]].nonzero()))
+    if len(candidates) == 0:
+        return None
+    return random.choice(candidates).item()
+
+
+def get_distance_matrix(x, num_cities=998):
+    x = torch.tensor(x)
+    x1, x2 = x[:,0:1], x[:,1:2]
+    d1 = x1 - (x1.T).repeat(num_cities,1)
+    d2 = x2 - (x2.T).repeat(num_cities,1)
+    distance_matrix = (d1**2 + d2**2)**0.5   # Euclidean Distance
+    return distance_matrix.numpy()
+
+
+def init_model(fname=None):
+    Q_net = QNet(EMBEDDING_DIMENSIONS, T=EMBEDDING_ITERATIONS_T).to(device)
+    optimizer = optim.Adam(Q_net.parameters(), lr=INIT_LR)
+    lr_scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=LR_DECAY_RATE)
+    
+    if fname is not None:
+        checkpoint = torch.load(fname)
+        Q_net.load_state_dict(checkpoint['model'])
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        lr_scheduler.load_state_dict(checkpoint['lr_scheduler'])
+    
+    Q_trainer = QTrainer(Q_net, optimizer, lr_scheduler)
+    return Q_trainer, Q_net, optimizer, lr_scheduler
+
+# Training Loop
+# TSP Data Load
+coords = np.array(pd.read_csv('2024_AI_TSP.csv', header=None))
+
+# make distance matrix
+W_np = get_distance_matrix(coords)
+
+# init Trainer, Model
+Q_trainer, Q_net, optimizer, lr_scheduler = init_model()
+
+# generate memory
+memory = Memory(MEMORY_CAPACITY)
+
+
+losses = []
+path_lengths = []
+found_solutions = dict()
+current_min_med_length = float('inf')
+
+
+for episode in range(NR_EPISODES):
+    
+    # tensor (distance matrix)
+    W = torch.tensor(W_np, dtype=torch.float32, requires_grad=False, device=device)
+    
+    # start node = 0
+    solution = [0]
+    
+    # current state
+    current_state = State(partial_solution=solution, W=W, coords=coords)
+    current_state_tsr = state2tens(current_state)
     
 
-
-class QTrainer():
-    def __init__(self, model, optimizer, lr_scheduler):
-        # QNetwork 인스턴스
-        self.model = model                  
-
-        # 학습에 활용할 QNetwork 학습 구성요소
-        self.optimizer = optimizer
-        self.lr_scheduler = lr_scheduler
-        self.loss_fn = nn.MSELoss()
+    # define state, state_tsrs(embedding), reward, action list
+    states = [current_state]
+    states_tsrs = [current_state_tsr] 
+    rewards = []
+    actions = []
+    
+    
+    # current value of epsilon
+    epsilon = max(MIN_EPSILON, (1-EPSILON_DECAY_RATE)**episode)
     
 
-    def predict(self, state_tsr, W):
-        # batch가 1인 인풋을 가정. inference 시 호출
-        with torch.no_grad():
-            estimated_q_value = self.model(state_tsr.unsqueeze(0), W.unsqueeze(0))
+    while not is_state_final(current_state):
+        
+        # select next node
+        if epsilon >= random.random():
+            next_node = get_next_neighbor_random(current_state)
+        else:
+            next_node, est_reward = Q_trainer.get_best_action(current_state_tsr, current_state)
+        
 
-        return estimated_q_value[0]
+        # append next node to solution
+        next_solution = solution + [next_node]
+
+        # calulate reward
+        reward = -(total_distance(next_solution, W) - total_distance(solution, W))
+        
+        
+        next_state = State(partial_solution=next_solution, W=W, coords=coords)
+        next_state_tsr = state2tens(next_state)
+        
+        states.append(next_state)
+        states_tsrs.append(next_state_tsr)
+        rewards.append(reward)
+        actions.append(next_node)
+        
+        
+        if len(solution) >= N_STEP_QL:
+            memory.remember(Experience(state=states[-N_STEP_QL],
+                                       state_tsr=states_tsrs[-N_STEP_QL],
+                                       action=actions[-N_STEP_QL],
+                                       reward=sum(rewards[-N_STEP_QL:]),
+                                       next_state=next_state,
+                                       next_state_tsr=next_state_tsr))
+            
+        if is_state_final(next_state):
+            for n in range(1, N_STEP_QL):
+                memory.remember(Experience(state=states[-n],
+                                           state_tsr=states_tsrs[-n], 
+                                           action=actions[-n], 
+                                           reward=sum(rewards[-n:]), 
+                                           next_state=next_state,
+                                           next_state_tsr=next_state_tsr))
+        
+        
+        current_state = next_state
+        current_state_tsr = next_state_tsr
+        solution = next_solution
+        
+
+        loss = None
+        if len(memory) >= BATCH_SIZE:
+
+            # sampling batch experience
+            experiences = memory.sample_batch(BATCH_SIZE)
+            
+            batch_states_tsrs = [e.state_tsr for e in experiences]
+            batch_Ws = [e.state.W for e in experiences]
+            batch_actions = [e.action for e in experiences]
+            batch_targets = []
+            
+
+            for i, experience in enumerate(experiences):
+                target = experience.reward
+                if not is_state_final(experience.next_state):
+                    _, best_q_value = Q_trainer.get_best_action(experience.next_state_tsr, experience.next_state)
+                    target += GAMMA * best_q_value
+                batch_targets.append(target)
                 
+            loss = Q_trainer.batch_update(batch_states_tsrs, batch_Ws, batch_actions, batch_targets)
+            losses.append(loss)
 
-    def get_best_action(self, state_tsr, state):
-        """ 주어진 state에 대해 최적의 greedy action을 선택하는 단계. 
-            다음 노드(aciton)의 index와 추정된 q_value.
-        """
-        W = state.W
-        estimated_q_value = self.predict(state_tsr, W)  
-        sorted_q_value_idx = estimated_q_value.argsort(descending=True)
-        
-        solution = state.partial_solution
-        
-        already_in = set(solution)
-        for idx in sorted_q_value_idx.tolist():
-            if (len(solution) == 0 or W[solution[-1], idx] > 0) and idx not in already_in:
-                return idx, estimated_q_value[idx].item()
-        
+    length = total_distance(solution, W)
+    path_lengths.append(length)
 
-    def batch_update(self, states_tsrs, Ws, actions, targets):
-        """ Batch단위의 (embedding of state, distance matrix, action, target_q_value)를 통해 Gradient를 통한 최적화를 수행하는 단계.
-            states_tsrs: list of (single) state tensors
-            Ws: list of W tensors
-            actions: list of actions taken
-            targets: list of targets (resulting estimated q_value after taking the actions)
-        """        
-        Ws_tsr = torch.stack(Ws).to(device)
-        xv = torch.stack(states_tsrs).to(device)
-        self.optimizer.zero_grad()
-        
-        estimated_q_value = self.model(xv, Ws_tsr)[range(len(actions)), actions]
-        
-        
-        loss = self.loss_fn(estimated_q_value, torch.tensor(targets, device=device))
-        loss_val = loss.item()
-        
-        loss.backward()
-        self.optimizer.step()        
-        self.lr_scheduler.step()
-        
-        return loss_val
+    if episode % 10 == 0:
+        print('Ep %d. Loss = %.3f, length = %.3f, epsilon = %.4f, lr = %.4f' % (
+            episode, (-1 if loss is None else loss), length, epsilon,
+            Q_trainer.optimizer.param_groups[0]['lr']))
+        found_solutions[episode] = (W.clone(), coords.copy(), [n for n in solution])
+
+# Generate Solutions
+solution = [0]
+current_state = State(partial_solution=solution, W=W, coords=coords)
+current_state_tsr = state2tens(current_state)
+
+while not is_state_final(current_state):
+    next_node, est_reward = Q_trainer.get_best_action(current_state_tsr, 
+                                                    current_state)
+    
+    solution = solution + [next_node]
+    current_state = State(partial_solution=solution, W=W, coords=coords)
+    current_state_tsr = state2tens(current_state)
+    
+print("Final solution : ", str(solution))
